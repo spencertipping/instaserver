c558075f70e638822b79adc8442d14b4524df55f
Spencer Tipping <spencer@spencertipping.com>
2011-01-03 11:10:05 -0700

    Added a self-modifying-Perl-based instaserver. Will document shortly.

--- instaserver.js
+++
@@ -1,4 +0,0 @@
-caterwaul.clone('std montenegro')(function () {
-  caterwaul.montenegro.server(8080) /se[_.file_extension_mimetypes /se[_.xml = 'text/xml', _.gif = 'image/gif'],
-                                        _.file('/', './').alias('/', '/index.html'),
-                                        _.proxy('/proxy/')]})();
